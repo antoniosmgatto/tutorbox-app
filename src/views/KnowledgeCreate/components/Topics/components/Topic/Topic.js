dde5a6c802016698e7bbdf15536e0639d23f5f4b
antoniomgatto <antonio.gatto@gobee.com.br>
2020-02-04 15:34:34 -0300

    creating a new component to manage input details for items

--- src/views/KnowledgeCreate/components/Topics/components/Topic/Topic.js
+++ src/views/KnowledgeCreate/components/Topics/components/Topic/Topic.js
@@ -1,8 +1,8 @@
 import React, { useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/styles'
-import { Typography, Button, TextField } from '@material-ui/core'
-import { HideElement } from "components"
+import { Typography, Button } from '@material-ui/core'
+import { ItemInput } from './component'
 
 const useStyles = makeStyles(theme => ({
   root: {},
@@ -22,18 +22,6 @@ const useStyles = makeStyles(theme => ({
   },
   addSubitem: {
     marginLeft: theme.spacing(4),
-  },
-  addItem: {
-    marginLeft: theme.spacing(1)
-  },
-  itemInputRoot: {
-    marginLeft: theme.spacing(1)
-  },
-  formActions: {
-    marginTop: theme.spacing(1)
-  },
-  saveButton: {
-    marginRight: theme.spacing(1)
   }
 }))
 
@@ -41,48 +29,44 @@ const Topic = props => {
   const { topic } = props
   const classes = useStyles()
   const [state, setState] = useState(topic)
-  const [openItemForm, setOpenItemForm] = useState(false)
-  const [newItem, setNewItem] = useState("")
-  const newItemRef = useRef()
-
-  useEffect(() => {
-    if (openItemForm) {
-      newItemRef.current.focus()
-    }
-  }, [openItemForm, state])
-
-  const handlerAddItem = () => {
-    setOpenItemForm(true)
-  }
-
-  const handleNewItemChanges = event => {
-    setNewItem(event.target.value)
-  }
-
-  const handlerSave = () => {
-    const itemsUpdated = [
-      ...state.items,
-      {
-        id: -1,
-        text: newItem,
-        subitems: []
-      }
-    ]
-
-    setState({...state, items: itemsUpdated })
-    setNewItem("")
-  }
-
-  const handlerCancel = () => {
-    setOpenItemForm(false)
-    setNewItem("")
-  }
 
   const handleRemoveItem = item => {
     const items = [...state.items]
     const index = items.indexOf(item)
     items.splice(index, 1)
     setState({ ...state, items: items })
+  }
+
+  const handleSaveItem = (value) => {
+    const itemsUpdated = [
+      ...state.items,
+      {
+        id: -1,
+        text: value,
+        subitems: []
+      }
+    ]
+
+    setState({
+      ...state,
+      items: itemsUpdated
+    })
+  }
+
+  const handleSaveSubitem = (value) => {
+    const itemsUpdated = [
+      ...state.items,
+      {
+        id: -1,
+        text: value,
+        subitems: []
+      }
+    ]
+
+    setState({
+      ...state,
+      items: itemsUpdated
+    })
   }
 
   return (
@@ -110,54 +94,11 @@ const Topic = props => {
                 </li>
               ))}
             </ul>
-
-            {item.subitems.length > 0 && (
-              <Button className={classes.addSubitem} color="primary">
-                Adicionar
-              </Button>
-            )}
           </li>
         ))}
       </ol>
 
-      <div>
-        <HideElement hide={openItemForm}>
-          <Button
-            className={classes.addItem}
-            color="primary"
-            onClick={handlerAddItem}
-          >
-            Clique aqui para adicionar um item
-          </Button>
-        </HideElement>
-        <HideElement hide={!openItemForm}>
-          <div className={classes.itemInputRoot}>
-            <TextField
-              className={classes.itemForm}
-              fullWidth
-              multiline
-              rowsMax={3}
-              variant="outlined"
-              onChange={handleNewItemChanges}
-              placeholder="Digite aqui..."
-              name="text"
-              value={newItem}
-              inputRef={newItemRef}
-            />
-            <div className={classes.formActions}>
-              <Button
-                className={classes.saveButton}
-                variant="outlined"
-                color="primary"
-                onClick={handlerSave}
-              >
-                Salvar
-              </Button>
-              <Button onClick={handlerCancel}>Cancelar</Button>
-            </div>
-          </div>
-        </HideElement>
-      </div>
+      <ItemInput openFormLabel="Clique aqui para adicionar um item" onSave={handleSaveItem} />
     </>
   )
 }
