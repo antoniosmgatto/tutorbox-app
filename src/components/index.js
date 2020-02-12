8ebc1265c9981ff9dc713b997769b9d7ef19e2d1
antoniomgatto <antonio.gatto@gobee.com.br>
2020-02-12 11:02:19 -0300

    adding SectionPaper component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ export { default as AttributeEditor } from './AttributeEditor';
 export { default as Comments } from './Comments';
 export { default as HideElement } from './HideElement';
 export { default as RouteWithLayout } from './RouteWithLayout';
-export { default as VideoStatus } from './VideoStatus';
+export { default as SectionPaper } from './SectionPaper';
 export { default as SimpleTextEditor } from './SimpleTextEditor';
+export { default as VideoStatus } from './VideoStatus';
