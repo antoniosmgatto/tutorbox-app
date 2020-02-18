107dfd0b442a030648318a4d4ff2ee6a57592480
antoniomgatto <antonio.gatto@gobee.com.br>
2020-02-18 12:58:15 -0300

    adding Todolist component

---
+++ src/components/Todolist/Todolist.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/styles'
+import clsx from 'clsx'
+import { TodoItem } from './components'
+import { List, Typography } from '@material-ui/core'
+import { Alert } from '@material-ui/lab'
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  todolist: {
+    minHeight: 511,
+  },
+  message: {
+
+  }
+}))
+
+const Todolist = props => {
+  const {
+    todolist,
+    className,
+    mode,
+    onToggleTodo,
+    onDeleteTodo,
+    ...otherProps
+  } = props
+  const classes = useStyles()
+  const enableCheckbox = mode === 'check'
+  const enableDelete = mode === 'edit'
+
+  const handleToggleTodo = todo => checked => {
+    onToggleTodo(todo, checked)
+  }
+
+  const handleDeleteTodo = todo => () => {
+    onDeleteTodo(todo)
+  }
+
+  return (
+    <div className={clsx(classes.root, className)} {...otherProps}>
+      <List
+        className={classes.todolist}
+      >
+      { todolist.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          enableCheckbox={enableCheckbox}
+          checked={todo.done}
+          onToggle={handleToggleTodo(todo)}
+          enableDelete={enableDelete}
+          onDelete={handleDeleteTodo(todo)}
+        />
+      ))}
+       { todolist.length === 0 && (
+         <Alert severity="info">Sem correções para este vídeo</Alert>
+       )}
+      </List>
+    </div>
+  )
+}
+
+Todolist.propTypes = {
+  todolist: PropTypes.array.isRequired,
+  className: PropTypes.string,
+  mode: PropTypes.oneOf(['edit', 'check']).isRequired,
+  onToggleTodo: PropTypes.func,
+  onDeleteTodo: PropTypes.func,
+}
+
+export default Todolist
---
+++ src/components/Todolist/components/TodoItem.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/styles'
+import { ListItem, ListItemText, ListItemSecondaryAction, IconButton, Checkbox } from '@material-ui/core'
+import { Delete as DeleteIcon } from '@material-ui/icons'
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  checkboxWrapper: {
+    paddingRight: theme.spacing(2),
+    alignItems: 'flex-start',
+  }
+}))
+
+const formatTime = seconds => {
+  const minutes = Math.floor(seconds / 60)
+  const diffInSeconds = seconds - (minutes * 60)
+  const minutesFormatted = String(minutes).padStart(2, '0')
+  const secondsFormatted = String(diffInSeconds).padStart(2, '0')
+  return `${minutesFormatted}:${secondsFormatted}`
+}
+
+const TodoItem = props => {
+  const {
+    todo,
+    checked,
+    enableCheckbox,
+    onToggle,
+    enableDelete,
+    onDelete
+  } = props
+  const classes = useStyles()
+  const formattedTime = todo.time ? formatTime(todo.time) : null
+
+  const handleChange = event => {
+    const { checked } = event.target
+    onToggle(checked)
+  }
+
+  const handleDelete = () => {
+    onDelete()
+  }
+
+  return (
+    <ListItem
+      className={classes.root}
+      divider
+    >
+      { enableCheckbox && <div className={classes.checkboxWrapper}>
+                            <Checkbox
+                              color="primary"
+                              checked={checked}
+                              value="true"
+                              onChange={handleChange}
+                            />
+                          </div>
+      }
+
+      <ListItemText
+        primary={todo.text}
+        secondary={formattedTime}
+      />
+
+      { enableDelete &&
+        <ListItemSecondaryAction>
+          <IconButton onClick={handleDelete}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      }
+    </ListItem>
+  )
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  checked: PropTypes.bool,
+  enableCheckbox: PropTypes.bool,
+  onToggle: PropTypes.func,
+  enableDelete: PropTypes.bool,
+  onDelete: PropTypes.func,
+}
+
+export default TodoItem
