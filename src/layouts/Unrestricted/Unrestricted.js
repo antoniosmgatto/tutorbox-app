f3d01a00ec1f660ef069cd9f4ed7b6774b7a647e
antoniomgatto <antonio.gatto@gobee.com.br>
2020-02-20 15:53:30 -0300

    moving generic structure from Authentication view  to Unrestricted layout

--- src/layouts/Unrestricted/Unrestricted.js
+++ src/layouts/Unrestricted/Unrestricted.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/styles'
-import { Container } from '@material-ui/core'
+import { Container, Grid, Paper, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100%'
-  }
+  },
+  gridContainer: {
+    height: '100vh',
+  },
+  paper: {
+    padding: 0,
+    width: '100%',
+    height: 400,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  section: {
+    padding: theme.spacing(2),
+    width: 320
+  },
+  header: {
+    textAlign: 'center',
+    padding: theme.spacing(4, 0)
+  },
+  childrenWrapper: {}
 }))
 
 const Unrestricted = props => {
@@ -15,7 +34,36 @@ const Unrestricted = props => {
   return (
     <main className={classes.root}>
       <Container>
-        {children}
+        <Grid
+          className={classes.gridContainer}
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid
+            item
+            md={8}
+            xs={12}
+          >
+            <Paper className={classes.paper}>
+              <section className={classes.section}>
+                <header className={classes.header}>
+                  <Typography
+                    variant="h2"
+                    component="h1"
+                  >
+                    Tutorbox
+                  </Typography>
+                </header>
+
+                <div className={classes.childrenWrapper}>
+                  { children }
+                </div>
+              </section>
+            </Paper>
+          </Grid>
+        </Grid>
       </Container>
     </main>
   )
--- src/views/Authentication/Authentication.js
+++ src/views/Authentication/Authentication.js
@@ -1,35 +1,16 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles'
-import { Paper, Typography, TextField, Button, Grid } from '@material-ui/core'
+import { TextField, Button } from '@material-ui/core'
 import { useState } from 'react'
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    height: '100vh',
-  },
-  paper: {
-    padding: 0,
-    width: '100%',
-    height: 400,
-    display: 'flex',
-    justifyContent: 'center'
-  },
-  section: {
-    padding: theme.spacing(2),
-    width: 320
-  },
-  header: {
-    textAlign: 'center',
-    padding: theme.spacing(4, 0)
-  },
   username: {},
   password: {
     marginTop: theme.spacing(2)
   },
 }))
 
-const Authentication = props => {
-  const {} = props
+const Authentication = _props => {
   const classes = useStyles()
   const [state, setState] = useState({})
 
@@ -47,79 +28,48 @@ const Authentication = props => {
   }
 
   return (
-    <Grid
-      className={classes.root}
-      container
-      direction="row"
-      justify="center"
-      alignItems="center"
-    >
-      <Grid
-        item
-        md={8}
-        xs={12}
+    <form>
+      <TextField
+        className={classes.username}
+        label="Endereço de email ou usuário"
+        name="username"
+        variant="outlined"
+        autoComplete="off"
+        fullWidth
+        onChange={handleChange}
+        autoFocus
+      />
+
+      <TextField
+        className={classes.password}
+        label="Senha"
+        name="password"
+        type="password"
+        variant="outlined"
+        autoComplete="off"
+        fullWidth
+        onChange={handleChange}
+      />
+
+      <div>
+        <Button
+          variant="text"
+          size="small"
+          onClick={handleForgetPasswordClick}
+        >
+          Esqueceu a senha?
+        </Button>
+      </div>
+
+      <Button
+        variant="contained"
+        color="primary"
+        fullWidth
+        onClick={handleAuthenticationClick}
       >
-        <Paper className={classes.paper}>
-
-              <section className={classes.section}>
-                <header className={classes.header}>
-                  <Typography
-                    variant="h2"
-                    component="h1"
-                  >
-                    Tutorbox
-                  </Typography>
-                </header>
-
-                <TextField
-                  className={classes.username}
-                  label="Endereço de email ou usuário"
-                  name="username"
-                  variant="outlined"
-                  autoComplete="off"
-                  fullWidth
-                  error={false}
-                  helperText=""
-                  onChange={handleChange}
-                  autoFocus
-                />
-
-                <TextField
-                  className={classes.password}
-                  label="Senha"
-                  name="password"
-                  type="password"
-                  variant="outlined"
-                  autoComplete="off"
-                  fullWidth
-                  error={false}
-                  helperText=""
-                  onChange={handleChange}
-                />
-
-                <div>
-                  <Button
-                    variant="text"
-                    size="small"
-                    color=""
-                    onClick={handleForgetPasswordClick}
-                  >
-                    Esqueceu a senha?
-                  </Button>
-                </div>
-
-                <Button
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                  onClick={handleAuthenticationClick}
-                >
-                  Entrar
-                </Button>
-              </section>
-        </Paper>
-      </Grid>
-    </Grid>
+        Entrar
+      </Button>
+    </form>
   )
 }
 
